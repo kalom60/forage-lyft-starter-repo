c9c3453e73ce28fe6c013e8761f9a4aeccbe71ec
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-10-31 17:28:05 +0300

    add needs service method

--- battery/nubbin_battery.py
+++ battery/nubbin_battery.py
@@ -1,4 +1,5 @@
 from battery import Battery
+from helper import curr_date
 
 
 class NubbinBattery(Battery):
@@ -6,3 +7,8 @@ class NubbinBattery(Battery):
         self.last_service_date = last_service_date
         self.current_date = current_date
         
+    def needs_service(self):
+        should_be_serviced = curr_date(self.last_service_date, 4)
+        if should_be_serviced < self.current_date:
+            return True
+        return False
--- battery/spindler_battery.py
+++ battery/spindler_battery.py
@@ -1,7 +1,14 @@
 from battery import Battery
+from helper import curr_date
 
 
 class SpindlerBattery(Battery):
     def __init__(self, last_service_date, current_date):
         self.last_service_date = last_service_date
         self.current_date = current_date
+
+    def needs_service(self):
+        should_be_serviced = curr_date(self.last_service_date, 2)
+        if should_be_serviced < self.current_date:
+            return True
+        return False
