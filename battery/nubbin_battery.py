fac1ff13ae8962e6224af6945c3a3e4ad2f69f7b
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-10-31 15:51:23 +0300

    Nubbin Battery class

--- battery/nubbin_battery.py
+++ battery/nubbin_battery.py
@@ -5,3 +5,4 @@ class NubbinBattery(Battery):
     def __init__(self, last_service_date, current_date):
         self.last_service_date = last_service_date
         self.current_date = current_date
+        
