1e2fbdd99686e7348e9038368f317610787bbbf5
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-11-01 21:49:34 +0300

    use the updated battery code

--- test/test_battery/test_spindler_battery.py
+++ test/test_battery/test_spindler_battery.py
@@ -5,7 +5,7 @@ from battery.spindler_battery import SpindlerBattery
 
 class TestSpindlerBattery(unittest.TestCase):
     def test_needs_service(self):
-        last_service_date = date.fromisoformat("2020-06-11")
+        last_service_date = date.fromisoformat("2019-06-11")
         current_date = date.fromisoformat("2022-11-01")
         battery = SpindlerBattery(last_service_date, current_date)
         self.assertTrue(battery.needs_service())  # Should be True
